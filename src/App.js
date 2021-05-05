927445f94a8c42daa4a13323e31c1651b2e8a679
Legend1998 <sumankuma.1997@gmail.com>
2021-05-05 15:28:37 +0530

    helmet added

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./Login";
 import Panel from "./Panel";
 import "./app.css";
+
 import { useStateValue } from "./StateProvider";
+import { Helmet } from "react-helmet";
 // import Signup from "./Signup";
+
+const TITLE = "Admin | TrackLab Distribution";
+
 function App() {
   const [{ user }] = useStateValue();
 
   return (
-    <div className="App  font-graphik ">
-      <Router>
-        <Switch>
-          {user ? (
-            <Route path="/panel">
-              <Panel />
-            </Route>
-          ) : null}
-          {/* 
+    <>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <div className="App  font-graphik ">
+        <Router>
+          <Switch>
+            {user ? (
+              <Route path="/panel">
+                <Panel />
+              </Route>
+            ) : null}
+            {/* 
           <Route path="/signup">
             <Signup />
           </Route> */}
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
